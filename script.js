e04c0a2d7bfc824861fd042393d9a9ce55286ff5
Yorqinovs <sunnatilloyorqinov2830@gmail.com>
2023-06-05 05:53:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// const box = document.querySelector("#box");
+// box.style.backgroundColor = "yellow";
+// box.style.height = "200px";
+// box.style.width = "200px";
+// box.style.marginBottom = "8%";
+// box.style.borderRadius = "10px";
+
+// const buttons = document.querySelectorAll("button");
+// buttons[1].style.width = "200px";
+
+// const circles = document.querySelectorAll(".circle");
+// circle[1].style.backgroundColor = "yellow";
+// circle[1].style.color = "black";
+
+// const circle = document.querySelectorAll(".circle");
+// circle[1].style.backgroundColor = "yellow";
+
+// const circle = document.querySelector(".circle");
+// circle.style.backgroundColor = "green";
+
+// box.style.cssText = "background-color: red; width: 100px; height: 100px";
+
+// const hearts = document.querySelectorAll(".heart");
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = "blue";
+// } // hammasiga o'zgartirish kiritamiz
+
+// const hearts = document.querySelectorAll(".heart");
+// hearts.forEach((item) => {
+//   item.style.backgroundColor = "gray";
+// }); // Bu uslu ancha qulay va osonroq usuli hisoblanadi
+
+// const btn = document.createElement("button"); // bu knopka uchun
+// //// const text = document.createTextNode("hello its me"); //bu textla uxhun
+// document.body.append(btn);
+
+// const circleWrapper = document.querySelector(".circle__wrapper");
+// const myCircle = document.createElement("div");
+// myCircle.classList.add("circle");
+// circleWrapper.append(myCircle); // oxiriga bitta shunaqa circle qo'shdik
+////---
+// const circles = document.querySelectorAll(".circle");
+// const circleWrapper = document.querySelector(".circle__wrapper");
+// const myCircle = document.createElement("div");
+// myCircle.classList.add("circle");
+// circleWrapper.before(myCircle); //undan oldin tepaga qo'yiladi
+// circleWrapper.after(myCircle); // undan keyin pastga tushadi
+// circles[0].after(myCircle); // 0-elementdan keyin qo'shadi
+// circles[1].remove(); // [] dagi 1-elementni o'chiradi
+// circles[1].replace(myCircle);
+// circleWrapper.append(myCircle); // yana bitta circle qo'shdik
+// myCircle.innerHTML = "E"; // o'sha cercleni ichiga yozamiz
+// myCircle.innerHTML = "<pre>E</pre>"; // innerHTML qanaqadur stil bersak ham bo'ladi
+// myCircle.textContent = "E"; // bu faqatgina textla uchun ishlatilinadi buni ichiga tag yozib bo'midi
+
+// circleWrapper.insertAdjacentHTML("afterbegin", "<pre>E</pre>"); // ichini boshiga kiradi
+// circleWrapper.insertAdjacentHTML("beforebegin", "<pre>E</pre>"); // oldiga tashqarisiga qo'yib beradi
+// circleWrapper.insertAdjacentHTML("afterend", "<pre>E</pre>"); // oxiriga qo'shib beradi
+// circleWrapper.insertAdjacentHTML("beforeend", "<pre>E</pre>"); // ichida oxiriga qo'shib beradi
